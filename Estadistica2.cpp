ab2db2aafd7738bcf9449be5bd86a3ec51a4efe4
Minepika55 <98884543+Minepika55@users.noreply.github.com>
2024-02-22 16:21:46 +0100

    Add files via upload

--- Estadistica2.cpp
+++ Estadistica2.cpp
@@ -18,117 +18,124 @@ int main()
     fstream fs;
     vector<Ciberware> ciberwares;
     int opcio;
+    int opcio2;
 
-    cout << "### Benvingut, Tria una Opcio ###" << endl;
-    cout << "1. Afegir Cyberware" << endl;
-    cout << "2. Mostrar Cyberware" << endl;
-    cout << "3. Mostrar Mitjana dels preus" << endl;
-    cout << "4. Mostrar La Moda de la perdua d'humanitat" << endl;
-    cin >> opcio;
+    do {
 
-    switch (opcio) {
-    case 1:
-    {
-        Ciberware c;
-        cout << "Nom del Cyberware:";
-        cin >> c.nom;
-        cout << "Local d'instalacio:";
-        cin >> c.instal;
-        cout << "Preu:";
-        cin >> c.preu;
-        cout << "Perdua d'Humanitat:";
-        cin >> c.perdua;
-        ciberwares.push_back(c);
-        fs.open("cyberdades.dat", ios::app | ios::binary);
-        if (fs.is_open()) {
-            fs.write((char*)&ciberwares[0], ciberwares.size() * sizeof(Ciberware));
-            fs.close();
-        }
-        else {
-            cout << "Error no s'ha pogut guardar al fitxer." << endl;
-        }
-        break;
-    }
-    case 2:
-    {
-        fs.open("cyberdades.dat", ios::in | ios::binary);
-        if (fs.is_open()) {
+
+        cout << "### Benvingut, Tria una Opcio ###" << endl;
+        cout << "1. Afegir Cyberware" << endl;
+        cout << "2. Mostrar Cyberware" << endl;
+        cout << "3. Mostrar Mitjana dels preus" << endl;
+        cout << "4. Mostrar La Moda de la perdua d'humanitat" << endl;
+        cin >> opcio;
+
+        switch (opcio) {
+        case 1:
+        {
             Ciberware c;
-            while (fs.read((char*)&c, sizeof(Ciberware))) {
-                cout << "------------------------" << endl;
-                cout << "Nom: " << c.nom << endl;
-                cout << "Instalacio: " << c.instal << endl;
-                cout << "Preu: " << c.preu << endl;
-                cout << "Perdua d'humanitat: " << c.perdua << endl;
-                cout << "------------------------" << endl;
-            }
-            fs.close();
-        }
-        else {
-            cout << "Error no es pot llegir el fitxer." << endl;
-        }
-        break;
-    }
-    case  3:
-    {
-        fs.open("cyberdades.dat", ios::in | ios::binary);
-        if (fs.is_open()) {
-            Ciberware c;
-            int sumaPreus = 0;
-            int comptador = 0;
-            while (fs.read((char*)&c, sizeof(Ciberware))) {
-                sumaPreus += c.preu;
-                comptador++;
-            }
-            fs.close();
-            if (comptador > 0) {
-                double mitjanaPreus = static_cast<double>(sumaPreus) / comptador;
-                cout << "La mitjana dels preus dels Cyberware es: " << mitjanaPreus << endl;
+            cout << "Nom del Cyberware:";
+            cin >> c.nom;
+            cout << "Local d'instalacio:";
+            cin >> c.instal;
+            cout << "Preu:";
+            cin >> c.preu;
+            cout << "Perdua d'Humanitat:";
+            cin >> c.perdua;
+            ciberwares.push_back(c);
+            fs.open("cyberdades.dat", ios::app | ios::binary);
+            if (fs.is_open()) {
+                fs.write((char*)&ciberwares[0], ciberwares.size() * sizeof(Ciberware));
+                fs.close();
             }
             else {
-                cout << "No es troben els preus." << endl;
+                cout << "Error no s'ha pogut guardar al fitxer." << endl;
             }
+            break;
         }
-        else {
-            cout << "Error no es pot llegir el fitxer." << endl;
-        }
-        break;
-        
-    }
-    case 4:
-    {
-        fs.open("cyberdades.dat", ios::in | ios::binary);
-        if (fs.is_open()) {
-            Ciberware c;
-            vector<int> valorsPerdua;
-            vector<int> freqPerdua;
-
-            while (fs.read((char*)&c, sizeof(Ciberware))) {
-                valorsPerdua.push_back(c.perdua);
+        case 2:
+        {
+            fs.open("cyberdades.dat", ios::in | ios::binary);
+            if (fs.is_open()) {
+                Ciberware c;
+                while (fs.read((char*)&c, sizeof(Ciberware))) {
+                    cout << "------------------------" << endl;
+                    cout << "Nom: " << c.nom << endl;
+                    cout << "Instalacio: " << c.instal << endl;
+                    cout << "Preu: " << c.preu << endl;
+                    cout << "Perdua d'humanitat: " << c.perdua << endl;
+                    cout << "------------------------" << endl;
+                }
+                fs.close();
             }
-            fs.close();
-
-            for (int i = 0; i < valorsPerdua.size(); ++i) {
-                int freq = std::count(valorsPerdua.begin(), valorsPerdua.end(), valorsPerdua[i]);
-                freqPerdua.push_back(freq);
+            else {
+                cout << "Error no es pot llegir el fitxer." << endl;
             }
-
-            int maxFreq = *std::max_element(freqPerdua.begin(), freqPerdua.end());
-            int modaPerdua = valorsPerdua[std::distance(freqPerdua.begin(), std::find(freqPerdua.begin(), freqPerdua.end(), maxFreq))];
-
-            cout << "La moda de la perdua d'humanitat es: " << modaPerdua << endl;
+            break;
         }
-        else {
-            cout << "Error no es pot llegir el fitxer." << endl;
+        case  3:
+        {
+            fs.open("cyberdades.dat", ios::in | ios::binary);
+            if (fs.is_open()) {
+                Ciberware c;
+                int sumaPreus = 0;
+                int comptador = 0;
+                while (fs.read((char*)&c, sizeof(Ciberware))) {
+                    sumaPreus += c.preu;
+                    comptador++;
+                }
+                fs.close();
+                if (comptador > 0) {
+                    double mitjanaPreus = static_cast<double>(sumaPreus) / comptador;
+                    cout << "La mitjana dels preus dels Cyberware es: " << mitjanaPreus << endl;
+                }
+                else {
+                    cout << "No es troben els preus." << endl;
+                }
+            }
+            else {
+                cout << "Error no es pot llegir el fitxer." << endl;
+            }
+            break;
+
         }
-        break;
+        case 4:
+        {
+            fs.open("cyberdades.dat", ios::in | ios::binary);
+            if (fs.is_open()) {
+                Ciberware c;
+                vector<int> valorsPerdua;
+                vector<int> freqPerdua;
 
-    }
+                while (fs.read((char*)&c, sizeof(Ciberware))) {
+                    valorsPerdua.push_back(c.perdua);
+                }
+                fs.close();
 
-    default:
-        cout << "Error torna a introduir una opcio.";
-        break;
-    }
+                for (int i = 0; i < valorsPerdua.size(); ++i) {
+                    int freq = std::count(valorsPerdua.begin(), valorsPerdua.end(), valorsPerdua[i]);
+                    freqPerdua.push_back(freq);
+                }
+
+                int maxFreq = *std::max_element(freqPerdua.begin(), freqPerdua.end());
+                int modaPerdua = valorsPerdua[std::distance(freqPerdua.begin(), std::find(freqPerdua.begin(), freqPerdua.end(), maxFreq))];
+
+                cout << "La moda de la perdua d'humanitat es: " << modaPerdua << endl;
+            }
+            else {
+                cout << "Error no es pot llegir el fitxer." << endl;
+            }
+            break;
+
+        }
+
+        default:
+            cout << "Error torna a introduir una opcio.";
+            break;
+        }
+        cout << "Vols fer algo mes? 1. Si 2. No" << endl;
+        cin >> opcio2;
+    } while (opcio2 == 1);
 
     return  0;
 }
